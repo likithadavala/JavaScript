cccdb53b8687449ca18ff184a7e5b3599be3cace
Likithadavala <likithadavala99@gmail.com>
2026-06-19 22:00:53 +0530

    Subtraction in Arithmetic Operation

---
+++ Arithmetic Operators/Subtraction/Subtraction.js
@@ -0,0 +1,7 @@
+function onclickSubtraction() {
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var SubResult = firstNumber + secondNumber;
+    document.getElementById("pResult").innerHTML = SubResult;
+}
