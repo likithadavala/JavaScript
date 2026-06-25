f0a16b9161082ac300af9f760eef3f354cd386ca
Likithadavala <likithadavala99@gmail.com>
2026-06-25 21:50:16 +0530

    Arithmetic Operators

---
+++ NumbersConcepts/ArithmeticOperators/ArithmeticOperators.js
@@ -0,0 +1,42 @@
+function addNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
+function subNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber - secondNumber;
+    alert(sum);
+    
+}
+function mulNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber * secondNumber;
+    alert(sum);
+    
+}
+function divNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber / secondNumber;
+    alert(sum);
+    
+}
+function remNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber % secondNumber;
+    alert(sum);
+    
+}
+
+
